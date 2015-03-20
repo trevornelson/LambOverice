5d1f07d2a8d68aa1c542e2e68da6888fa3934642
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-20 19:27:14 -0400

    Updated answer spec for test users to be valid users

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe "Answer" do
-    let(:question_user) { User.create(username: 'test_user', password_digest: '12345', email: 'test_user@test.com') }
-    let(:other_user) { User.create(username: 'test_user2', password_digest: '12345', email: 'test_user@test.com') }
-    let(:question) { Question.create(user_id: question_user.id, title: 'How do I run tests with Rspec?') }
-    let(:answer) { Answer.create(user_id: other_user.id, question_id: question.id, content: 'Go look on the internet.') }
+    let(:question_user) { User.create(username: 'test_user', password: '12345', email: 'test_user@test.com') }
+    let(:other_user) { User.create(username: 'test_user2', password: '12345', email: 'test_user2@test.com') }
+    let(:question) { Question.create(user: question_user, title: 'How do I run tests with Rspec?') }
+    let(:answer) { Answer.create(user: other_user, question: question, content: 'Go look on the internet.') }
 
   describe '#vote_count' do
 
@@ -13,12 +13,12 @@ describe "Answer" do
     end
 
     it 'should decrease after a down vote' do
-      Vote.create(user_id: question_user.id, voteable: answer, direction: 'Down')
+      Vote.create(user: question_user, voteable: answer, direction: 'Down')
       expect(answer.vote_count).to eq(0)
     end
 
     it 'should increase after an up vote' do
-      Vote.create(user_id: question_user.id, voteable: answer, direction: 'Up')
+      Vote.create(user: question_user, voteable: answer, direction: 'Up')
       expect(answer.vote_count).to eq(2)
     end
 
