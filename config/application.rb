c687812580e54675a45ad7844c99d1ad1d53ba7e
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-20 08:45:41 -0400

    Updated config to compile Bootstrap assets in production

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module StackOverflow
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Required for Bootstrap to work when deployed to Heroku
+    config.assets.initialize_on_precompile = false
   end
 end
