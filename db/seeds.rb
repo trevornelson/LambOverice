8b6e5e9f80148edd40ec3078dd1962f774d5d6d4
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-19 20:10:05 -0400

    Finished database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create user for manual testing
+User.create(username: 'halal', password: '12345',email: 'lamb@rice.com')
+
+['Javascript', 'Ruby', 'Python', 'HTML', 'CSS'].each do |category|
+  Category.create(name: category)
+end
+
+3.times do
+question_poster = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, email: Faker::Internet.email)
+
+  2.times do
+    random_category_id = Category.all.map{|cat| cat.id}.sample
+    question = question_poster.questions.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, category_id: random_category_id)
+    3.times do
+      answer_poster = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, email: Faker::Internet.email)
+      answer = question.answers.create(user_id: answer_poster.id, content: Faker::Lorem.paragraph)
+      3.times do
+        answer_comment_poster = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, email: Faker::Internet.email)
+        answer.comments.create(user_id: answer_comment_poster.id, content: Faker::Lorem.paragraph)
+      end
+    end
+    4.times do
+      comment_poster = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, email: Faker::Internet.email)
+      question.comments.create(user_id: comment_poster.id, content: Faker::Lorem.paragraph)
+    end
+  end
+end
+
+
+
