3cc1672ffaa377f2afc254fbef6ebed47cab314f
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-20 12:37:49 -0400

    Fixed merge conflict in routes config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
 
+  root 'questions#index'
+  resources :sessions
+  resources :users
+
+
   resources :answers, except: [:new, :create, :show, :index]
 
   resources :questions do
