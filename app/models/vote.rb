251d48f1a9f9775f67f9a3136fce839b43c78583
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-20 19:50:37 -0400

    Passed vote model spec

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -2,4 +2,12 @@ class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :voteable, polymorphic: true
   validates_uniqueness_of :voteable_id, scope: :user_id, message: "You already voted"
+  validate :check_self_voting
+
+  def check_self_voting
+    if voteable.user.id == user_id
+      errors.add(:user_id, "Can't vote on your own post")
+    end
+  end
+
 end
