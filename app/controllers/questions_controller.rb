a49b6884837239714f25304816c16d122088bfbb
Leo Rogers <leo.a.rogers@gmail.com>
2015-03-20 16:55:32 -0400

    added create method to question controller along with new question form

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -11,6 +11,23 @@ class QuestionsController < ApplicationController
     @answer = Answer.new
   end
 
-  def create
+  def new
+    @question = Question.new
   end
+
+  def create
+    category = Category.find_or_create_by(name: params[:question][:category_name])
+    params[:question][:category_id] = category.id
+    params[:question][:user_id] = session[:user_id]
+    Question.create(question_params)
+    redirect_to curr_user_path
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :content, :category_name, :category_id, :user_id)
+  end
+
+
 end
