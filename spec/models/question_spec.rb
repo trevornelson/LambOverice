2dc407c57b38d574baa588d34bcdf3202dfc9092
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-20 19:27:45 -0400

    Implemented question vote_count functionality

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 describe Question do
-  let(:question_user) { User.create(username: 'test_user', password_digest: '12345', email: 'test_user@test.com') }
-  let(:other_user) { User.create(username: 'test_user2', password_digest: '12345', email: 'test_user@test.com') }
-  let(:question) { Question.create(user_id: 1, title: 'How do I run tests with Rspec?') }
+  let(:question_user) { User.create(username: 'test_user', password: '12345', email: 'test_user@test.com') }
+  let(:other_user) { User.create(username: 'test_user2', password: '12345', email: 'test_user2@test.com') }
+  let(:question) { Question.create(user: question_user, title: 'How do I run tests with Rspec?') }
 
   describe '#vote_count' do
     it 'should initialize with 1 vote' do
@@ -11,12 +11,12 @@ describe Question do
     end
 
     it 'should decrease after a down vote' do
-      Vote.create(user_id: other_user.id, voteable: question, direction: 'Down')
+      Vote.create(user: other_user, voteable: question, direction: 'Down')
       expect(question.vote_count).to eq(0)
     end
 
     it 'should increase after an up vote' do
-      Vote.create(user_id: other_user.id, voteable: question, direction: 'Up')
+      Vote.create(user: other_user, voteable: question, direction: 'Up')
       expect(question.vote_count).to eq(2)
     end
 
