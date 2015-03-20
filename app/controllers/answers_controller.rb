a6e037383d159a4aa0ca891d3d06f91406b8bdf0
Rayanb <rayanbouts@gmail.com>
2015-03-20 14:55:30 -0400

    answers spec

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,11 +1,37 @@
 class AnswersController < ApplicationController
 
-  def index
-  end
-
-  def show
+  def new
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new
   end
 
   def create
+    question = Question.find(params[:question_id])
+    answer = question.answers.new(content: params[:answer][:content], user_id: session[:user_id])
+    if answer.save
+      redirect_to question
+    else
+      # Add ajax to display the error message getting back from validations
+      redirect_to new_question_answer_path(question.id)
+    end
   end
+
+  def edit
+    @answer = Answer.find(params[:id])
+  end
+
+  def update
+    answer = Answer.find(params[:id])
+    question = answer.question
+    if answer.update(content: params[:answer][:content])
+      redirect_to question
+    else
+      redirect_to :back
+    end
+  end
+
+  def delete
+    answer = Answer.find(params[:id])
+  end
+
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,15 +1,39 @@
 require 'rails_helper'
+require 'pp'
 
 describe AnswersController do
-  describe 'GET routes' do
-    it "GET questions/x/answers"
-    it "GET questions/x/answers/y"
-  end
+  let(:user){User.create(username: "Leo Dongchul", password: '123', email: "therio@gmail.com")}
+  let(:question) {Question.create(title: "Who is DongChul ?", content: "He is mysterious and mindful, who is he, really ?", category_id: 1, user: user)}
 
-  describe "POST routes" do
-    it "POST /questions/q/answers/new"
-    it "PUT /answers/x"
-    it "DELETE /answers/x"
+  context 'hey' do
+    before(:each) do
+      user
+      question
+      session[:user_id] = user.id
+    end
+
+    describe 'GET routes' do
+
+      it "shows the form of a new answer to a specific question" do
+        get :new, :question_id => question.id
+        expect(response).to be_ok
+      end
+
+    end
+
+    describe "POST routes" do
+
+      it "POST /questions/x/answers/new" do
+        post :create, {:question_id => question.id, :answer => {content: "I think Dongchul is a very rare specimen, happy all the time and not caring about anyone around him, he is an example to every human being."}}
+        expect(response).to redirect_to(question)
+      end
+
+      it 'doesnt record a wrong answer' do
+        post :create, {:question_id => question.id, answer: {content:"" }}
+        expect(response).to redirect_to(new_question_answer_path(question.id))
+      end
+
+    end
   end
 
 end
