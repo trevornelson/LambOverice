6e0533ef4bb667c241a6005a6e8c5157315e8c7c
Rayanb <rayanbouts@gmail.com>
2015-03-20 17:00:32 -0400

    test comments controller to check that a comment is correctly saved

---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+require 'pp'
+
+describe CommentsController do
+
+
+  context "the comments tests"
+
+    let(:user){User.create(username: "Leo Dongchul", password: '123', email: "therio@gmail.com")}
+    let(:question) {Question.create(title: "Who is DongChul ?", content: "He is mysterious and mindful, who is he, really ?", category_id: 1, user: user)}
+    let(:answer) {Answer.create(question: question, user: user, content: "Makes sense")}
+
+
+    describe 'Commenting on a question' do
+
+      it 'creates a comment to a specific question' do
+        user
+        question
+        session[:user_id] = user.id
+        post :create, {:question_id => question.id, comment: {content: "I feel like this answer is accurate"} }
+        expect(response.body).to eq("I return a JSON object that says I saved myself")
+      end
+
+    end
+
+    describe 'Commenting on an answer' do
+
+      it 'creates a comment to a specific answer' do
+        answer
+        session[:user_id] = user.id
+        post :create, {:answer_id => answer.id, comment: {content: "I feel like this answer is accurate"} }
+        expect(response.body).to eq("I return a JSON object that says I saved myself")
+      end
+
+    end
+end
