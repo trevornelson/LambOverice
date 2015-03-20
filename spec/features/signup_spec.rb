4c16ef29762f0ca430c09b74a8af9727635a5ff0
Leo Rogers <leo.a.rogers@gmail.com>
2015-03-20 12:09:17 -0400

    added signin and signup feature tests

---
+++ spec/features/signup_spec.rb
@@ -0,0 +1,17 @@
+require 'capybara/rspec'
+require 'rails_helper'
+
+feature "user creates an account" do
+
+  scenario "user signs up for account" do
+    visit '/users/new'
+    within('#signup') do
+      fill_in 'Username', with: "Chriseldo"
+      fill_in 'Email', with: "Chiseldo@gmail.com"
+      fill_in 'Password', with: "1234"
+    end
+    click_button "Create Account"
+    expect(page).to have_content 'Test Userpage'
+  end
+
+end
