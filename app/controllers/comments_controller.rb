c68653c569b3728278b8cc2a85cef19586261fd7
Rayanb <rayanbouts@gmail.com>
2015-03-20 19:02:56 -0400

    enable comments on each question, and each answer

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,11 +6,8 @@ class CommentsController < ApplicationController
     answer_id   = params[:answer_id]
     question_id ? commentable = Question.find(question_id) : commentable = Answer.find(answer_id)
     comment = Comment.new(content: params[:comment][:content], commentable: commentable, user: user )
-    if comment.save
-      render text: "I return a JSON object that says I saved myself"
-    else
-      render text: " I return an error message"
-    end
+    comment.save
+    redirect_to :back
   end
 
 end
