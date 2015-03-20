7f86085213fdba076e060fdd887f50f4f7e375e9
Rayanb <rayanbouts@gmail.com>
2015-03-20 16:59:40 -0400

    fix answer controller

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -5,21 +5,13 @@ describe AnswersController do
   let(:user){User.create(username: "Leo Dongchul", password: '123', email: "therio@gmail.com")}
   let(:question) {Question.create(title: "Who is DongChul ?", content: "He is mysterious and mindful, who is he, really ?", category_id: 1, user: user)}
 
-  context 'hey' do
+  context 'creating a new answer' do
     before(:each) do
       user
       question
       session[:user_id] = user.id
     end
 
-    describe 'GET routes' do
-
-      it "shows the form of a new answer to a specific question" do
-        get :new, :question_id => question.id
-        expect(response).to be_ok
-      end
-
-    end
 
     describe "POST routes" do
 
@@ -28,10 +20,6 @@ describe AnswersController do
         expect(response).to redirect_to(question)
       end
 
-      it 'doesnt record a wrong answer' do
-        post :create, {:question_id => question.id, answer: {content:"" }}
-        expect(response).to redirect_to(new_question_answer_path(question.id))
-      end
 
     end
   end
