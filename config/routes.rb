34aa298d38c49d7f28f0a4c9022d9e198501c0c9
Leo Rogers <leo.a.rogers@gmail.com>
2015-03-20 00:39:11 -0400

    added users, sessions, and root url to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+
   resources :answers, except: [:new, :create, :show, :index]
 
   resources :questions do
     resources :answers, only: [:new, :create]
   end
+
+  root 'sessions#new'
+  resources :sessions
+  resources :users
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
