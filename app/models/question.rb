76ab7e2e6138ce3d0acebc9a148fbd8b3dab5d60
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-21 19:14:57 -0400

    Fixed merge conflicts when rebasing

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,13 +1,10 @@
 class Question < ActiveRecord::Base
-  scope :find_with_included_relations, -> (id){includes(:user, :category, comments: :user, answers: [{comments: :user}, :user]).find(id)}
 
   belongs_to :user
   has_many :votes, as: :voteable
   has_many :answers
   has_many :comments, as: :commentable
   belongs_to :category
-<<<<<<< HEAD
-=======
 
   scope :with_all_relations, -> (question_id) { includes(:user,
                                                          :category,
@@ -24,5 +21,5 @@ class Question < ActiveRecord::Base
   def decr_vote_count
     self.decrement!(:vote_count)
   end
->>>>>>> Created question#with_all_relations scope and refactored for readability
+
 end
