5dedcb0ce20cc664e30b5d4344d150879b04bb55
Rayanb <rayanbouts@gmail.com>
2015-03-21 15:07:24 -0400

    add search functionality

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,12 +1,9 @@
 class Question < ActiveRecord::Base
+  scope :find_with_included_relations, -> (id){includes(:user, :category, comments: :user, answers: [{comments: :user}, :user]).find(id)}
+
   belongs_to :user
   has_many :votes, as: :voteable
   has_many :answers
   has_many :comments, as: :commentable
   belongs_to :category
-
-  def vote_count
-    votes = self.votes
-    1 + (votes.where(direction: 'Up').count - votes.where(direction: 'Down').count)
-  end
 end
