90c5223d9d91ca441db2fd34f3f4261e4ef00bac
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-21 20:42:26 -0400

    Updated seed file with standardized passwords

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,21 @@ User.create(username: 'halal', password: '12345',email: 'lamb@rice.com')
 end
 
 3.times do
-question_poster = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, email: Faker::Internet.email)
+question_poster = User.create(username: Faker::Internet.user_name, password: '12345', email: Faker::Internet.email)
 
   2.times do
     random_category_id = Category.all.map{|cat| cat.id}.sample
     question = question_poster.questions.create(title: Faker::Lorem.sentence, content: Faker::Lorem.paragraph, category_id: random_category_id)
     3.times do
-      answer_poster = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, email: Faker::Internet.email)
+      answer_poster = User.create(username: Faker::Internet.user_name, password: '12345', email: Faker::Internet.email)
       answer = question.answers.create(user_id: answer_poster.id, content: Faker::Lorem.paragraph)
       3.times do
-        answer_comment_poster = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, email: Faker::Internet.email)
+        answer_comment_poster = User.create(username: Faker::Internet.user_name, password: '12345', email: Faker::Internet.email)
         answer.comments.create(user_id: answer_comment_poster.id, content: Faker::Lorem.paragraph)
       end
     end
     4.times do
-      comment_poster = User.create(username: Faker::Internet.user_name, password: Faker::Internet.password, email: Faker::Internet.email)
+      comment_poster = User.create(username: Faker::Internet.user_name, password: '12345', email: Faker::Internet.email)
       question.comments.create(user_id: comment_poster.id, content: Faker::Lorem.paragraph)
     end
   end
