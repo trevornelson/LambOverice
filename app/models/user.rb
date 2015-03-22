a402709f5e3231163e58de40297e9dd74a3ffaf0
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-21 20:42:58 -0400

    Created user#karma model methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,25 @@ class User < ActiveRecord::Base
   has_many :answers
   has_many :comments
 
+  def karma
+    question_karma + answer_karma + comment_karma
+  end
+
+  private
+
+  def question_karma
+    karma_weight = 2
+    questions.map(&:vote_count).inject(0, &:+) * karma_weight
+  end
+
+  def answer_karma
+    karma_weight = 4
+    answers.map(&:vote_count).inject(0, &:+) * karma_weight
+  end
+
+  def comment_karma
+    karma_weight = 0.5
+    comments.inject(0, &:+) * karma_weight
+  end
+
 end
