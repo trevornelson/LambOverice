713b6d389646abe298c7eb28ec822bfd8c2a3aaf
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-22 21:37:59 -0400

    Created initial markdown widget

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,3 +1,28 @@
+$(document).ready(function(){
+
+	MarkdownWidget.listener('#question-content', '#question-preview');
+
+	MarkdownWidget.listener('#question-title', '#preview-title');
+
+});
+
+
+var MarkdownWidget = function(){
+	var listener = function(input_field, preview_div){
+		$(input_field).on('focus', function(e){
+			$(input_field).keyup(function(){
+				$(preview_div).text($(input_field).val());
+			})
+		});
+	}
+
+	return {
+		listener: listener
+	}
+}();
+
+
+
 var Question = function(params){
   this.id = params.id;
   this.title = params.title;
