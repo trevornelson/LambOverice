d1467379bdcce41699f646b8db58a197d2c5c02a
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-25 13:43:53 -0400

    Added initial react render call

---
+++ app/assets/javascripts/react_comments.js
@@ -0,0 +1,16 @@
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <li>
+        <h2>Trevor Nelson</h2>
+        <p>Some test text to render</p>
+      </li>
+    );
+  }
+});
+
+
+React.render(
+  <CommentBox />,
+  document.getElementById('question-comment-box')
+);
