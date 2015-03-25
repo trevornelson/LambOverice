3ab814dae7de764afde92fbbff74e405ff421169
Trevor Nelson <trevor.nelson1@gmail.com>
2015-03-25 14:38:44 -0400

    Implemented stubbed comments data for react

--- app/assets/javascripts/react_comments.js.jsx
+++ app/assets/javascripts/react_comments.js.jsx
@@ -1,17 +1,63 @@
 /** @jsx React.DOM **/
-var CommentBox = React.createClass({
-  render: function() {
-    return (
-      <li>
-        <h2>Trevor Nelson</h2>
-        <p>Some test text to render</p>
-      </li>
-    );
-  }
+$(document).ready(function(){
+  var data = [
+    {user: 'Trevor Nelson', text: 'Heres the first comment!'},
+    {user: 'Crab Daddy', text: 'CrAbZ R DeLiSh'}
+  ];
+
+  var CommentBox = React.createClass({
+    render: function() {
+      return (
+        <div className="commentbox">
+          <h2>Comments</h2>
+          <CommentList data={data} />
+        </div>
+      );
+    }
+  });
+
+  var CommentList = React.createClass({
+    render: function() {
+      var commentNodes = this.props.data.map(function (comment) {
+        return (
+          <Comment user={comment.user} text={comment.text} />
+        );
+      });
+      return (
+        <div className="commentList">
+          {commentNodes}
+        </div>
+      );
+    }
+  });
+
+  var Comment = React.createClass({
+    render: function() {
+      return (
+        <li className="list-group-item">
+          <p>{this.props.text}</p>
+          <a href="#">
+            <strong>{this.props.user}</strong>
+          </a>
+        </li>
+      );
+    }
+
+  });
+
+  var CommentForm = React.createClass({
+    render: function() {
+      return (
+        <li class="list-group-item">
+
+        </li>
+      );
+    }
+
+  });
+
+  React.render(
+    <CommentBox />,
+    document.getElementById('question-comment-box')
+  );
 });
-
-
-React.render(
-  <CommentBox />,
-  document.getElementById('question-comment-box')
-);
